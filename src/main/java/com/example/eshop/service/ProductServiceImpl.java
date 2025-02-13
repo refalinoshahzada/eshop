56853d898ab28e73aa1bd27c7aacd5caf719791b
refalinoshahzada <ref4gha@gmail.com>
2025-02-14 00:20:24 +0700

    [FEAT]: Added interface for deleteById method and implemented it into ProductServiceImpl.java

--- src/main/java/com/example/eshop/service/ProductServiceImpl.java
+++ src/main/java/com/example/eshop/service/ProductServiceImpl.java
@@ -44,4 +44,9 @@ public class ProductServiceImpl implements ProductService {
     public Product update(Product product) {
         return productRepository.update(product);
     }
+
+    @Override
+    public void deleteById(String productId) {
+        productRepository.deleteById(productId);
+    }
 }
