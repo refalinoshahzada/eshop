d7777fb12534b457c5a715e897c5b8df30d4da05
refalinoshahzada <ref4gha@gmail.com>
2025-02-13 18:08:08 +0700

    [FEAT]: Created Product.java in the module package

---
+++ src/main/java/com/example/eshop/model/Product.java
@@ -0,0 +1,11 @@
+package com.example.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
