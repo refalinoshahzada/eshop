e0cd5d6f3dc9a3b5c8cc4777d839d98423935f6b
refalinoshahzada <ref4gha@gmail.com>
2025-02-13 22:34:01 +0700

    [FEAT]: Implementing update product and find by ID methods

--- src/main/java/com/example/eshop/repository/ProductRepository.java
+++ src/main/java/com/example/eshop/repository/ProductRepository.java
@@ -20,4 +20,22 @@ public class ProductRepository {
         return productData.iterator();
     }
 
+    public Product update(Product updatedProduct) {
+        for (int i = 0; i < productData.size(); i++) {
+            if (productData.get(i).getProductId().equals(updatedProduct.getProductId())) {
+                productData.set(i, updatedProduct);
+                return updatedProduct;
+            }
+        }
+        return null;
+    }
+
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
 }
