33de98f320266e9a32abacbbbc0b514443749a9e
benz <ref4gha@gmail.com>
2025-02-28 19:01:36 +0700

    [FEAT]: Added RepositoryInterface.java as an application of LSP and as a generic extension repository

---
+++ src/main/java/com/example/eshop/repository/RepositoryInterface.java
@@ -0,0 +1,11 @@
+package com.example.eshop.repository;
+
+import java.util.List;
+
+public interface RepositoryInterface<T> {
+    T create(T item);
+    List<T> findAll();
+    T findById(String id);
+    T update(T item);
+    void delete(String id);
+}
