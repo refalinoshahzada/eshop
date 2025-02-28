0c6de899b88ca94584d1de65d123997f386d4b67
benz <ref4gha@gmail.com>
2025-02-28 17:56:04 +0700

    [FIXED]: Fixed unresolved commits

---
+++ src/main/java/com/example/eshop/controller/CarController.java
@@ -0,0 +1,66 @@
+package com.example.eshop.controller;
+
+import com.example.eshop.model.Car;
+import com.example.eshop.service.CarService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+    private final CarService carservice;
+
+    public CarController(CarService carService) {
+        this.carservice = carService;
+    }
+
+    @GetMapping("/createCar")
+    public String createCarPage(Model model) {
+        Car car = new Car();
+        model.addAttribute("car", car);
+        return "createCar";
+    }
+
+    @PostMapping("/createCar")
+    public String createCarPost(@ModelAttribute Car car, Model model) {
+        carservice.create(car);
+        return "redirect:listCar";
+    }
+
+    @GetMapping("/listCar")
+    public String carListPage(Model model) {
+        List<Car> allCars = carservice.findAll();
+        model.addAttribute("cars", allCars);
+        return "carList";
+    }
+
+    @GetMapping("/editCar/{carId}")
+    public String editCarPage (@PathVariable String carId, Model model) {
+        Car car = carservice.findById(carId);
+
+        if (car == null) {
+            return "redirect:/car/listCar";
+        }
+
+        model.addAttribute("car", car);
+        return "editCar";
+    }
+
+    @PostMapping("/editCar")
+    public String editCarPost (@ModelAttribute Car car) {
+        System.out.println(car.getCarId());
+        carservice.update(car.getCarId(), car);
+
+        return "redirect:listCar";
+    }
+
+    @PostMapping("/deleteCar")
+    public String deleteCar (@RequestParam("carId") String carId) {
+        carservice.deleteCarById(carId);
+        return "redirect:listCar";
+    }
+}
--- src/main/java/com/example/eshop/service/CarServiceImpl.java
+++ src/main/java/com/example/eshop/service/CarServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.eshop.service;
 import com.example.eshop.model.Car;
-import com.example.eshop.repository.CarRepositoryInterface;
+import com.example.eshop.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
@@ -11,7 +11,7 @@ import java.util.List;
 public class CarServiceImpl implements CarService {
 
     @Autowired
-    private CarRepositoryInterface carRepository;
+    private CarRepository carRepository;
 
     @Override
     public Car create (Car car) {
